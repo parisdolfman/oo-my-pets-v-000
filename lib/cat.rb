f16479e3ab7690e277e9a8fd15adb7626e1d8871
Paris Dolfman <parisdolfman@gmail.com>
2018-11-19 20:58:32 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,15 @@
 class Cat
 
-attr_reader :name
+attr_reader :name #name passed as initialized argument cannot be changed only read what is written
+
+attr_accessor :mood #object can change as it reader & writer method
 
 def initialize(name)
 @name = name
+@mood = "nervous"
 end
+
+
 
 
 
